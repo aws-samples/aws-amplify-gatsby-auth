cfc5dac0a5e1574d096b398f203b28bec8dd8463
infodaddy <github.com@infodaddy.com>
2020-09-29 10:47:26 -0700

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -75,7 +75,7 @@ const styles = {
     display: 'flex', flexDirection: 'column'
   },
   button: {
-    backgroundColor: 'rebeccapurple', padding: '15px 7px', cursor: 'pointer', textAlign: 'center', marginBottom: 10
+    backgroundColor: '#3b5998', padding: '15px 7px', cursor: 'pointer', textAlign: 'center', marginBottom: 10
   },
   buttonText: {
     color: 'white'
