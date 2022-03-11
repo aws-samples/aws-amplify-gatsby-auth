f35779d535a5733b47adc4561c1cc6373ed9e584
Jeff <jeff@cilutions.com>
2022-03-11 07:46:23 -0500

    Update Home.js
    Changed title of home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Home = () => <div>
-  <h1>Home</h1>
+  <h1>Home with Change</h1>
 	<p>You are now logged in! <Link to="/app/profile">View profile</Link></p>
 	<p>Now go build something great and deploy it using the <a href="https://console.amplify.aws">AWS Amplify Console</a></p>
 	</div>
